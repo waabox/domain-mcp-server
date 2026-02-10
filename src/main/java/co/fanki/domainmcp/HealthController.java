ca61841a70ef5d232923c048f8573e2d53b73fbc
Emiliano Arango (waabox) <emiliano@fanki.co>
2026-02-10 07:59:20 -0300

    Move HealthController to root package

--- src/main/java/co/fanki/domainmcp/HealthController.java
+++ src/main/java/co/fanki/domainmcp/HealthController.java
@@ -1,4 +1,4 @@
-package co.fanki.domainmcp.config;
+package co.fanki.domainmcp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

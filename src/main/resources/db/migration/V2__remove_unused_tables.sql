10fd813b589932f3f2c0e4ff6455e1057e605ccf
Emiliano Arango (waabox) <emiliano@fanki.co>
2026-02-10 08:05:39 -0300

    Fix duplicate HealthController and migration order

--- src/main/resources/db/migration/V2__remove_unused_tables.sql
+++ src/main/resources/db/migration/V2__remove_unused_tables.sql
@@ -1,9 +1,10 @@
 -- Remove unused tables from initial schema
 -- These were planned but not implemented
+-- Drop in correct order due to foreign key dependencies
 
-DROP TABLE IF EXISTS analysis_results;
-DROP TABLE IF EXISTS api_endpoints;
 DROP TABLE IF EXISTS business_rules;
+DROP TABLE IF EXISTS api_endpoints;
 DROP TABLE IF EXISTS contracts;
 DROP TABLE IF EXISTS database_schemas;
 DROP TABLE IF EXISTS source_files;
+DROP TABLE IF EXISTS analysis_results;
